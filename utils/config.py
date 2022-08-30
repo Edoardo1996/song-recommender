93be1d0b45fac5c08a23be37bfa8e0664b9ad4f3
Edoardo Nervo <edoardonervo96@gmail.com>
2022-08-30 19:01:13 +0200

    modify config.py

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+for el in environ:
+    globals()[f"{el}"] = environ.get(el)
