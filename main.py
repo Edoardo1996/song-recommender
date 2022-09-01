cef9d4fd97dac3fa73de63f1e3da7f66798d53bf
Edoardo Nervo <edoardonervo96@gmail.com>
2022-09-01 17:25:41 +0200

    implement first version of playlist analyser and tqdm handling

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from utils.songdb import SpotifyClient, retrieve_playlists
 import utils.config as config
+import tqdm
+import pandas as pd
+import os
 
 
 def main():
@@ -10,7 +13,21 @@ def main():
     # Retrieve all playlists ids
     playlists_ids = retrieve_playlists(sp, "spotify")
 
-    
+    # Get all songs from the playlists
+    # if os.path.exists(config.DB_PATH):
+    #     songs = pd.read_csv(config.DB_PATH)
+    # else:
+    #     songs = pd.DataFrame()
+    #     for id in tqdm(playlists_ids[:5]):
+    #         songs = pd.concat([songs, analyze_playlist('spotify', id)], axis=0, ignore_index=True)
+    #     # Clean the duplicates track
+    #     songs = songs.set_index("track_id")
+    #     songs = songs.drop_duplicates()
+    #     songs.to_csv("songs.csv")
+
+    # Retrieve song features
+
+
     
 
 
--- utils/songdb.py
+++ utils/songdb.py
@@ -4,8 +4,9 @@ API
 """
 import warnings
 import spotipy
+import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
-
+from tqdm import tqdm
 
 class SpotifyClient:
     """
@@ -52,18 +53,57 @@ class SpotifyClient:
                 raise Exception("Authorization Failes")
 
 
-def retrieve_playlists(sp: spotipy.Spotify, user: str, offset: int = None) -> list[str]:
+def retrieve_playlists(sp: spotipy.Spotify, creator: str, offset: int = None) -> list[str]:
     """
     Retrieve playlists ids created by a certain user
     """
+    def generator():
+        while True:
+            yield
+
     playlists_ids = list()
-    playlists = sp.user_playlists(user)
-    while playlists:
+    playlists = sp.user_playlists(creator)
+    for _ in (pbar := tqdm(generator())):
+        pbar.set_description("Extracting playlists ids")
         for i, playlist in enumerate(playlists['items']):
             playlists_ids.append(playlist['id'])
         if playlists['next']:
             playlists = sp.next(playlists)
         else:
-            playlists = None
+            break
 
     return playlists_ids
+
+def analyze_playlist(sp: spotipy.Spotify, creator, playlist_id) -> pd.DataFrame:
+    
+    # Create empty dataframe
+    playlist_features_list = ["artist","album","track_name",  "track_id","danceability","energy","key","loudness","mode", "speechiness","instrumentalness","liveness","valence","tempo", "duration_ms","time_signature"]
+    
+    playlist_df = pd.DataFrame(columns = playlist_features_list)
+    
+    # Loop through every track in the playlist, extract features and append the features to the playlist dLf
+    
+    playlist = sp.user_playlist_tracks(creator, playlist_id)["items"]
+    for track in playlist:
+        # Create empty dict
+        playlist_features = {}
+        # Get metadata
+        playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
+        playlist_features["album"] = track["track"]["album"]["name"]
+        playlist_features["track_name"] = track["track"]["name"]
+        playlist_features["track_id"] = track["track"]["id"]
+        try:
+            playlist_features["genres"] = sp.artist(track["track"]["album"]["artists"][0]["id"])["genres"][0] # TODO: add genre handling
+        except:
+            playlist_features["genres"] = 'unknown'
+        
+        # Get audio features
+        audio_features = sp.audio_features(playlist_features["track_id"])[0]
+        for feature in playlist_features_list[4:]:
+            playlist_features[feature] = audio_features[feature]
+        
+        # Concat the dfs
+        track_df = pd.DataFrame(playlist_features, index = [0])
+        playlist_df = pd.concat([playlist_df, track_df], ignore_index = True)
+        
+    return playlist_df
